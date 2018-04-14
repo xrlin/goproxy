411523c605229c5b19a59b44dd93676d90ff1d2c
xrlin <xrlin@m.scnu.edu.cn>
2018-04-14 19:13:16 +0800

    refactor code.

--- proxy.go
+++ proxy.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"github.com/pkg/errors"
 	"io"
 	"log"
 	"net"
 	"net/http"
-	"strings"
 	"sync"
 )
 
@@ -21,64 +21,77 @@ func (p *proxy) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	p.handleHTTP(w, req)
 }
 
+func (p *proxy) getIP(req *http.Request) (ip string, err error) {
+	ip, _, err = net.SplitHostPort(req.RemoteAddr)
+	return
+}
+
+func (p proxy) addHeader(sourceHeader http.Header, header http.Header) {
+	for key, value := range header {
+		for _, v := range value {
+			sourceHeader.Add(key, v)
+		}
+	}
+}
+
 // Handle normal http connection
 func (p *proxy) handleHTTP(w http.ResponseWriter, req *http.Request) {
 	transport := http.DefaultTransport
 	newReq := new(http.Request)
 	*newReq = *req
-	if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
-		if prior, ok := newReq.Header["X-Forwarded-For"]; ok {
-			clientIP = strings.Join(prior, ", ") + ", " + clientIP
-		}
-		newReq.Header.Set("X-Forwarded-For", clientIP)
+	if clientIP, err := p.getIP(req); err == nil {
+		p.addHeader(newReq.Header, http.Header{"X-Forwarded-For": []string{clientIP}})
 	}
 	resp, err := transport.RoundTrip(newReq)
 	if err != nil {
 		w.WriteHeader(http.StatusBadGateway)
 		return
 	}
-	for key, value := range resp.Header {
-		for _, v := range value {
-			w.Header().Add(key, v)
-		}
-	}
+	p.addHeader(w.Header(), resp.Header)
 	w.WriteHeader(resp.StatusCode)
 	io.Copy(w, resp.Body)
 	resp.Body.Close()
 }
 
+func (p proxy) pipeConnection(dst *net.Conn, src *net.Conn, wg *sync.WaitGroup) {
+	io.Copy(*dst, *src)
+	wg.Done()
+}
+
+func (p proxy) convertToHijackConn(w http.ResponseWriter) (conn net.Conn, err error) {
+	hj, ok := w.(http.Hijacker)
+	if !ok {
+		err = errors.New("Hijacking not supported")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// called before Hijack() called, otherwise will block the connection
+	w.WriteHeader(http.StatusOK)
+	conn, _, err = hj.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+	}
+	return
+}
+
 // Use tunnel to proxy https、ws and other proto..
 func (p *proxy) handleTunnel(w http.ResponseWriter, req *http.Request) {
 	conn, err := net.Dial("tcp", req.Host)
-	eof := sync.WaitGroup{}
 	if err != nil {
 		w.Write([]byte(err.Error()))
 		w.WriteHeader(http.StatusServiceUnavailable)
 		return
 	}
 	defer conn.Close()
-	hj, ok := w.(http.Hijacker)
-	if !ok {
-		http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
-		return
-	}
-	// called before Hijack() called, otherwise will block the connection
-	w.WriteHeader(http.StatusOK)
-	source, _, err := hj.Hijack()
+	source, err := p.convertToHijackConn(w)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusServiceUnavailable)
 		return
 	}
 	defer source.Close()
+	eof := sync.WaitGroup{}
 	eof.Add(2)
-	go func() {
-		io.Copy(conn, source)
-		eof.Done()
-	}()
-	go func() {
-		io.Copy(source, conn)
-		eof.Done()
-	}()
+	go p.pipeConnection(&conn, &source, &eof)
+	go p.pipeConnection(&source, &conn, &eof)
 	eof.Wait()
 }
 
